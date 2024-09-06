98849705c2f7e7abac21ddef953a3dec4b80a038
Luis Herrera <a.22308051281207@cbtis128.edu.mx>
2024-09-06 09:58:57 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+"""Luis Herrera
+Trabajando con cadenas"""
+
+nombre="Luis"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["L","U","I","S"]
+for chicas in MisNovias:
+    print(chicas)
+    
+    print("--otro ejemplo--")
+    b = "Luis Herrera"
+print(b[2:5])
+print("--a mayusculas--")
+a="Luis Herrera"
+print(a.upper())
+print("--concatenar--")
+a="Maria"
+b="Perez"
+c=a+" "+b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,16 @@
+import math
+edad=17    # 17 se guardas en edad
+
+nombre="Luis Herrera"
+print("tu edad es ",edad)
+print("te llamas ", nombre)
+print("-- tipo de variables --")
+x = str(7)    # x will be '3'
+y = int(3.1415)    # y will be 3
+z=float(7)
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas--")
+x = math.sqrt(64)
+print(x)
